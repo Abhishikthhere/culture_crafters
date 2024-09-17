d0f1def9c75317e81f8080a6856c00d1a724234a
Abhishikthhere <143127069+Abhishikthhere@users.noreply.github.com>
2024-09-18 01:32:40 +0530

    Delete Culture_Crafters directory

--- Culture_Crafters/node2.js
+++
@@ -1,4 +0,0 @@
-// Simple interaction for language selection
-document.getElementById('language-dropdown').addEventListener('change', function() {
-    alert('Language changed to: ' + this.value);
-});
